b4f6fce5c707db45fd0c11f244ac1505399ecb23
juliamrosa <jewelmrosa@gmail.com>
2021-02-21 15:06:44 -0600

    reorganize file structure

---
+++ app.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    var aboutMeNames = [ "On top of a mountain in South Korea", "Singing at the Red Lion in Manhattan", "Babson Alumni Event", "Santorini, Greece", "Sky Diving on Long Island", "Sunset in Southampton", "Oil Paintings", "Oil Paintings"];
+    var aboutMeImages = [ "southkorea.jpg", "red-lion.jpg", "babson.jpg", "greece.jpg", "sky-diving.jpg", "beach-sunset.png", "girl-1.png", "girl-2.png"];
+
+    var currentIndex = 0;
+
+    function changeNameAndImage() {
+        $("#carousel-items img").attr("src", "images/" + aboutMeImages[currentIndex]);
+        $("#carousel-items img").attr("alt", aboutMeNames[currentIndex]);
+        $(".carousel h2").text(aboutMeNames[currentIndex]);
+    }
+
+    changeNameAndImage();
+
+    $(".right-handle").on("click", function(){
+        if (currentIndex === 7) {
+            currentIndex = 0;
+
+            changeNameAndImage();
+        } else {
+            currentIndex = currentIndex + 1;
+
+            changeNameAndImage();
+        }
+    });
+
+    $(".left-handle").on("click", function(){
+        console.log(".left-handle")
+        if (currentIndex === 0) {
+            currentIndex = 7;
+            
+            changeNameAndImage();
+        } else {
+            currentIndex = currentIndex - 1;
+
+            changeNameAndImage();
+        }
+    });
+
+    // $(".right-handle").on("click", function(){
+    //     if (currentIndex === 7) {
+    //         return;
+    //     } else {
+    //         currentIndex = currentIndex + 1;
+
+    //         changeNameAndImage();
+    //     }
+    // });
+
+    // $(".left-handle").on("click", function(){
+    //     console.log(".left-handle")
+    //     if (currentIndex === 0) {
+    //         return;
+    //     } else {
+    //         currentIndex = currentIndex - 1;
+
+    //         changeNameAndImage();
+    //     }
+    // });
+
+
+
+});
