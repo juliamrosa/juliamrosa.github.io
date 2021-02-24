19f6e584a3c7e4c784ae4972fb12dd7b1019b08e
juliamrosa <jewelmrosa@gmail.com>
2021-02-23 20:37:08 -0600

    fixing image issues

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var aboutMeNames = [ "On Stage", "Singing at the Red Lion in Manhattan", "Trio", "On top of a mountain in South Korea",  "Babson Alumni Event", "Sky Diving on Long Island", "Sunset in Southampton", "Me and my Girl", "Cheesin'"];
-    var aboutMeImages = [ "singing-profile.jpg", "red-lion.jpg", "band.jpg", "southkorea.jpg", "babson.jpg", "sky-diving.jpg", "beach-sunset.png", "rooney.jpg", "about-me.jpg"];
+    var aboutMeNames = [ "On Stage", "Singing at the Red Lion in Manhattan", "Trio", "On top of a mountain in South Korea", "Santorini Greece",  "Babson Alumni Event", "Sky Diving on Long Island", "Sunset in Southampton", "Me and my Girl", "Cheesin'"];
+    var aboutMeImages = [ "singing-profile.jpg", "red-lion.jpg", "band.jpg", "southkorea.jpg", "greece.jpg", "babson.jpg", "sky-diving.jpg", "beach-sunset.png", "rooney.jpg", "about-me.jpg"];
 
     var currentIndex = 0;
 
@@ -13,7 +13,7 @@ $(document).ready(function() {
     changeNameAndImage();
 
     $(".right-handle").on("click", function(){
-        if (currentIndex === 7) {
+        if (currentIndex === 9) {
             currentIndex = 0;
 
             changeNameAndImage();
@@ -27,7 +27,7 @@ $(document).ready(function() {
     $(".left-handle").on("click", function(){
         console.log(".left-handle")
         if (currentIndex === 0) {
-            currentIndex = 7;
+            currentIndex = 9;
             
             changeNameAndImage();
         } else {
